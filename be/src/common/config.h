9c912ca7b50f97fd06e8c35b08f9db81dd297659
predator4ann <yunlong.sun@hotmail.com>
2025-10-20 18:26:03 +0800

    fix: 修改 config

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1682,7 +1682,7 @@ CONF_mInt32(avro_reader_buffer_size_bytes, "8388608");
 CONF_mBool(cdc_enable, "false");
 // Whether to stream CDC sending while collecting to reduce peak memory.
 // When enabled, we still preserve strict per-tablet ordering.
-CONF_mBool(cdc_streaming_send, "false");
+CONF_Bool(cdc_streaming_send, "true");
 // CDC Kafka configuration
 // Kafka broker list, comma separated
 CONF_String(cdc_kafka_brokers, "localhost:9092");
